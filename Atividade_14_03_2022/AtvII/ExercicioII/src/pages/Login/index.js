086c5110847cb8da92e8e7e82be1cd0b510fa195
Sophia M Silveira <sophiasilveira2003@gmail.com>
2022-03-14 17:53:00 -0300

    att- atvII

---
+++ Atividade_14_03_2022/AtvII/ExercicioII/src/pages/Login/index.js
@@ -0,0 +1,168 @@
+import React from 'react';
+import {View,
+        Text,
+        StyleSheet,
+        SafeAreaView,
+        StatusBar,
+        TouchableOpacity} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Fontisto';
+
+function Login({navigation}){
+    return(
+      <SafeAreaView style = {{flex:1}}>
+        <StatusBar hidden = {false} translucent={true} backgroundColor={'transparent'}/>
+        <View style = {[styles.container]}>
+          <View style = {[styles.beforeContainer]} >
+            <TouchableOpacity style = {[styles.iconContainer]}
+                  onPress = {() =>
+                 navigation.navigate("Home")}>
+              <Icon name ='arrow-left' size={25} color='#fff'/>
+            </TouchableOpacity>
+            <View style = {[styles.insideBeforeContainer]}>
+              <Icon style = {[styles.iconTwo]} name ='search' size={25} color='#fff'/>
+            </View>
+          </View>
+          <View style = {[styles.containerOne]}></View>
+          <View style = {[styles.containerTwo]}>
+           <View style = {[styles.boxOne]}>
+             <Text style = {[styles.textName]}>Your Name</Text>
+           </View>
+           <View style = {[styles.boxTwo]}>
+             <View style = {[styles.littleBox, styles.aline]}>
+               <Icon name ='messenger' size={30} color='#000'/>
+             </View>
+             <View style = {[styles.littleBox, styles.aline]}>
+               <Icon name ='commenting' size={30} color='#000'/>
+             </View>
+           </View>
+           <View style = {[styles.boxThree]}>
+             <Text style = {[styles.marginLeft]}>Your basic info 1</Text>
+             <Text style = {[styles.marginLeft, styles.marginTop]}>Your basic info 2</Text>
+           </View>
+           <View style = {[styles.boxFour]}>
+             <Text style = {[ styles.finalLineText]}>ABOUT</Text>
+             <Text style = {[ styles.finalLineText]}>PHOTOS</Text>
+             <Text style = {[ styles.finalLineText]}>FRIENDS</Text>
+           </View>
+          </View>
+          <View style = {styles.containerThree}></View>
+        </View>
+      </SafeAreaView>
+    );
+
+};
+  const styles = StyleSheet.create ({
+    container: {
+     flex: 1,
+    },
+
+    beforeContainer: {
+      height: 75,
+      width: '100%',
+      backgroundColor: '#3B5998',
+      flexDirection: 'row',
+    },
+
+    iconContainer: {
+     marginTop: 32,
+     marginLeft: 15,
+    },
+
+    insideBeforeContainer:{
+      height: 45,
+      width: '77%',
+      flexDirection: 'row',
+      marginLeft: 35,
+      marginTop: 20,
+      borderBottomColor: '#fff',
+      borderBottomWidth: 1.5,
+    },
+
+    iconTwo: {
+     marginTop: 12,
+    },
+
+    containerOne:{
+      flex: 1.8,
+      backgroundColor: '#bfc4dc',
+    },
+
+    containerTwo:{
+      flex:3.2,
+      alignItems: 'center',
+      backgroundColor: '#fff'
+    },
+    containerThree:{
+      width: '25.5%',
+      height: 160,
+      backgroundColor: '#dfe1ee',
+      position: 'absolute',
+      marginTop: 255,
+      marginLeft: '30%',
+      borderColor: '#fff',
+      borderWidth: 3,
+    },
+    boxOne:{
+     flex: 4,
+     width: '90%',
+     borderBottomColor: 'black',
+     borderBottomWidth: 1,
+     justifyContent: 'flex-end',
+     alignItems: 'center',
+    },
+    boxTwo:{
+     flex: 1.5,
+     width: '100%',
+     flexDirection: 'row',
+     alignItems: 'center',
+     justifyContent: 'space-around',
+    },
+    boxThree:{
+     flex: 1.7,
+     borderTopColor: 'black',
+     width: '90%',
+     borderTopWidth: 1,
+     justifyContent: 'center',
+    },
+    boxFour:{
+     flex: 1,
+     width: '100%',
+     flexDirection: 'row',
+     alignItems: 'center',
+     justifyContent: 'space-around',
+     backgroundColor: '#f5f5f5',
+    },
+
+    littleBox:{
+      width: 50,
+      height: 50,
+    },
+
+    textName: {
+     color: '#000',
+     fontSize: 33,
+     marginBottom: 15,
+    },
+
+    finalLineText: {
+      fontWeight: 'bold',
+      fontSize: 18,
+    },
+
+    marginLeft:{
+     marginLeft: 40,
+     fontSize: 16,
+    },
+    marginTop: {
+     marginTop: 10,
+    },
+
+    aline: {
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    },
+
+ });
+
+export default Login;
